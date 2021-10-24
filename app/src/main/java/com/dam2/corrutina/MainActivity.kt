a08c57b29c06fc64e3c40757dad357832ece53b3
Damian Nogueiras <damian@danielcastelao.org>
2021-10-24 19:27:47 +0200

    Proyecto Empty, con un botón y un textView

---
+++ app/src/main/java/com/dam2/corrutina/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.dam2.corrutina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val miBoton: Button = findViewById(R.id.miBoton)
+
+        miBoton.setOnClickListener {
+            lanzarCorrutina()
+        }
+
+    }
+
+    private fun lanzarCorrutina() {}
+
+}
