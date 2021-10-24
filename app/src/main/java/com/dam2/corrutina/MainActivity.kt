994a01320f3cf1e89e8df9e1d4589c4cfc0f5532
Damian Nogueiras <damian@danielcastelao.org>
2021-10-24 19:44:34 +0200

    Añadimos una segunda tarea que rellena el cuadro de texto, mientras la primera tarea esta detenida

--- app/src/main/java/com/dam2/corrutina/MainActivity.kt
+++ app/src/main/java/com/dam2/corrutina/MainActivity.kt
@@ -28,19 +28,32 @@ class MainActivity : AppCompatActivity() {
     private fun lanzarCorrutina() {
         val miTexto: TextView = findViewById(R.id.miTextView)
 
-        // 'launch' se encarga de crear una corrutina
+        // 'launch' se encarga de crear una corrutina Task
         // vamos a poder identificarla con 'job'
         val job = GlobalScope.launch(Dispatchers.Main) {
             // llamamos a una funcion que estara dentro de la corrutina
             // en esta funcion habrá un delay()
             suspendingTask(miTexto)
             }
+
+        // añadimos otra corrutina Task2 identificada como job2
+        val job2 = GlobalScope.launch(Dispatchers.Main) {
+            suspendingTask2(miTexto)
         }
+    }
+
+    // segunda Tarea Task2
+    // se ejecuta en paralelo con la primera Task
+    suspend fun suspendingTask2(miTexto: TextView) {
+        // espero 1,5sg
+        delay(1500L)
+        miTexto.text = "TASK 2"
+    }
 
     // la declaramos 'suspend' porque tendrá un Delay
     suspend fun suspendingTask(miTexto: TextView) {
         miTexto.text = "Corrutina"
-        // paramos la corrutina pero no la UI principal
+        // paramos 3sg la corrutina pero no la UI principal
         delay(3000L)
         miTexto.text = "Hello Coroutine!"
     }
