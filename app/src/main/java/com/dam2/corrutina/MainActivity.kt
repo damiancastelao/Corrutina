11e9feecc4a29ccf49e81b2ae49e873b19ae5ba4
Damian Nogueiras <damian@danielcastelao.org>
2021-10-24 19:34:55 +0200

    Añadimos 'launch' para crear una corrutina

--- app/src/main/java/com/dam2/corrutina/MainActivity.kt
+++ app/src/main/java/com/dam2/corrutina/MainActivity.kt
@@ -3,6 +3,10 @@ package com.dam2.corrutina
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +18,20 @@ class MainActivity : AppCompatActivity() {
 
         // declaramos el listener del boton
         miBoton.setOnClickListener {
+            // llamamos a una funcion que lanzara la corrutina
             lanzarCorrutina()
         }
 
     }
+    // declaramos la funcion que lanzara la corrutina
+    private fun lanzarCorrutina() {
+        val miTexto: TextView = findViewById(R.id.miTextView)
 
-    private fun lanzarCorrutina() {}
+        // 'launch' se encarga de crear una corrutina
+        // vamos a poder identificarla con 'job'
+        val job = GlobalScope.launch(Dispatchers.Main) {
+
+        }
+    }
 
 }
